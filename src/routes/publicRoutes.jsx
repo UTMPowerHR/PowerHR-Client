90a8942a538200dfef09ba04e3d5c23d0a0db570
Naim Jalil <muhammadnaim370@gmail.com>
2024-05-31 06:41:39 +0800

    PWRC-8 Register Applicant

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -10,9 +10,9 @@ const publicRoutes = (
             <Route path="" element={<div>Home</div>} />
             <Route path={PATHS.AUTH.FORGOT_PASSWORD} element={<Authentcation.ForgotPassword />} />
             <Route path={PATHS.AUTH.RESET_PASSWORD} element={<Authentcation.ResetPassword />} />
-        </Route>
-        <Route element={<CheckLogin />}>
-            <Route element={<Layout />}>
+            <Route path={PATHS.AUTH.REGISTER} element={<Authentcation.Register />} />
+
+            <Route element={<CheckLogin />}>
                 <Route path={PATHS.AUTH.LOGIN} element={<Authentcation.Login />} />
             </Route>
         </Route>
