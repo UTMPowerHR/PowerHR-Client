5ece0213136ea0c09e63572c70d2847234b97a4b
Ahmad Saifudin <101683512+Saifdn@users.noreply.github.com>
2024-11-13 03:21:55 +0800

    Resolve conflict hrSidebar.jsx

--- src/layouts/sidebar/hrSidebar.jsx
+++ src/layouts/sidebar/hrSidebar.jsx
@@ -45,12 +45,12 @@ const hrSection = [
                         path: PATHS.COMPANY.MANAGE.EMPLOYEES,
                     },
                     {
-
                         title: 'Terminate Employee',
                         path: PATHS.COMPANY.FIREEMPLOYEE,
+                    },
+                    {
                         title: 'Rehire Employees',
                         path: PATHS.COMPANY.REHIRE,
-
                     },
                 ],
             },
