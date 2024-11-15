311873cd201275a6e5a28e72a61c6b2e30c45131
HisyamFirdaus03 <hisyamfirdaus100@gmail.com>
2024-11-16 01:11:33 +0800

    Added retrieve employee details in transfer knowledge for HR interface

--- src/pages/company/TransferKnowledge.jsx
+++ src/pages/company/TransferKnowledge.jsx
@@ -1,17 +1,28 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import TableDocument from './components/tableDocument';
+import { setEmployees } from '@features/company/companySlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useGetEmployeesQuery } from '@features/company/companyApiSlice';
 
 function TransferKnowledge() {
     const { employeeId } = useParams();  // Get employeeId from the URL
     const [selectedEmployee, setSelectedEmployee] = useState(null);
-    console.log(employeeId);
+    const user = useSelector((state) => state.auth.user);
+    const employees = useSelector((state) => state.company.employees);
+    const dispatch = useDispatch();
+    const { data, isSuccess } = useGetEmployeesQuery(user.company);
+
+    useEffect(() => {
+        if (isSuccess) {
+            dispatch(setEmployees(data.employees));
+        }
+    }, [data, dispatch, isSuccess]);
+
     useEffect(() => {
         if (employeeId) {
-            // Mock fetching employee data based on employeeId
-            console.log(`Fetching data for employee with ID: ${employeeId}`);
-            // Simulate setting the employee
-            setSelectedEmployee({ id: employeeId, name: 'John Doe' }); // Replace with actual data fetching logic
+            const employee = employees.find((employee) => employee._id == employeeId);
+            setSelectedEmployee({ id: employee._id, name: employee.firstName + " " + employee.lastName, email: employee.email }); // Replace with actual data fetching logic
         }
     }, [employeeId]);
 
@@ -19,7 +30,7 @@ function TransferKnowledge() {
         <>
 
             {selectedEmployee ? (
-                <TableDocument selectedEmployeeId={selectedEmployee.id} />
+                <TableDocument selectedEmployee={selectedEmployee} />
             ) : (
                 <p>Loading employee data...</p> // Loading state until employee data is available
             )}
