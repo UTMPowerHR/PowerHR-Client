9078083c94ec1946e3cb651ad050d068cdaa08c8
liokockhock2003 <liokockhock@gmail.com>
2024-11-28 09:12:04 +0800

    remove sending email

--- src/pages/company/components/tableFireEmployee.jsx
+++ src/pages/company/components/tableFireEmployee.jsx
@@ -103,17 +103,6 @@ function TableEmployees() {
         setConfirmationOpen(true);
     };
 
-    // Function to convert file to Base64
-    const fileToBase64 = (file) => {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = (error) => reject(error);
-        });
-    };
-
-
     // Modified confirm termination function
     const confirmTerminateEmployee = async () => {
         setConfirmationOpen(false);
